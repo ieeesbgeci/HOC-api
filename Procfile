42f1ff201a4899c02089399e50ff15db2de8f91e
v4zha <arvin_j4848@protonmail.com>
2022-02-03 19:31:07 +0530

    update

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: chmod u+x server.sh && ./server.sh
 web: ./target/release/discord-db-api
--- server.sh
+++
@@ -1,3 +0,0 @@
-
-cargo install diesel_cli --no-default-features --features postgres
-diesel migration run
